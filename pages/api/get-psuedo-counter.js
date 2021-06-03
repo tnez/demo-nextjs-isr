677a774d289341489d2ef5052ac288ce8dafbdca
Travis Nesland <tnesland@gmail.com>
2021-06-03 15:53:22 -0400

    send default response on timeout

--- pages/api/get-psuedo-counter.js
+++ pages/api/get-psuedo-counter.js
@@ -1,7 +1,9 @@
 const URL = 'https://demo-nextjs-isr.vercel.app/api/get-psuedo-counter'
+const TIMEOUT_THRESHOLD = 5000
 
 export default async function (_, res) {
   res.setHeader('Cache-Control', 's-maxage=2, stale-while-revalidate')
+  setTimeout(sendDefaultResponse(res), TIMEOUT_THRESHOLD)
   try {
     const previousResponse = await (await fetch(URL)).json()
     console.log('previousResponse', previousResponse)
@@ -9,4 +11,8 @@ export default async function (_, res) {
   } catch (err) {
     res.status(200).json({ counter: 0, error: err.message })
   }
+}
+
+function sendDefaultResponse(res) {
+  res.status(200).json({ counter: 0, error: 'Timed Out' })
 }
